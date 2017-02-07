a0d21b979145151ef4ea0dbde4dc72cbcb5a5808
LN STEMpunks <cade@lnstempunks.org>
2017-02-07 18:39:39 -0500

    Adding in code for testing the ultrasonic serial sensor

--- src-java/src/team3966/robot/subsystems/Subsystems.java
+++ src-java/src/team3966/robot/subsystems/Subsystems.java
@@ -3,21 +3,22 @@ package team3966.robot.subsystems;
 import team3966.robot.hardware.DistanceSensor;
 import team3966.robot.values.IDs;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import team3966.robot.hardware.UltrasonicSerial;
 public class Subsystems {
 
 	public Drive drive;
 	public OI OI;
 	public DistanceSensor ultrasonic;
+	public UltrasonicSerial ports;
 	
 	public Subsystems() {
 		drive = new Drive();
 		OI = new OI();
 		ultrasonic = new DistanceSensor(IDs.ultrasonic_0);
+		ports = new UltrasonicSerial();
 	}
 	
 	public void dumpInfo () {
-		SmartDashboard.putNumber("Distance Sensor", ultrasonic.getDistance());
-		SmartDashboard.putNumber("Average Volts", ultrasonic.getAverageVoltage());
-		SmartDashboard.putNumber("Volts", ultrasonic.getVoltage());
+		//SmartDashboard.putString("Distance: ", ports.PortReadout());
 	}
 }
