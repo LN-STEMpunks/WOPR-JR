142d7ae53945ac8175a00e43506b99236258f703
LN STEMpunks <cade@lnstempunks.org>
2017-02-08 18:04:05 -0500

    Keep reading in member variable

--- src-java/src/team3966/robot/hardware/UltrasonicSerial.java
+++ src-java/src/team3966/robot/hardware/UltrasonicSerial.java
@@ -5,12 +5,13 @@ import edu.wpi.first.wpilibj.SerialPort.Port;
 
 public class UltrasonicSerial {
 	SerialPort serial = null;
+	String distanceString = "No data";
 
 	public String PortReadout() {
 		try {
-			if (serial == null) {
+			if(serial == null) {
 				serial = new SerialPort(9600, Port.kOnboard);
-				serial.setTimeout(0.020);
+				serial.setTimeout(0.1);
 			}
 			// byte [] buf = serial.read(8);
 			// return new String(buf);
@@ -18,10 +19,9 @@ public class UltrasonicSerial {
 			if (serial.getBytesReceived() > 0) {
 
 				String readout = serial.readString();
-				return readout;
-			} else {
-				return "No data";
-			}
+				this.distanceString = readout;
+			} 
+			return this.distanceString;
 		} catch (Exception e) {
 			return "Got an error";
 		}
