3dd46948c930f49b03d00eb168679dbddd17a301
LN STEMpunks <cade@lnstempunks.org>
2017-02-07 19:57:23 -0500

    serial ultrasonic cleanup and error handling

--- src-java/src/team3966/robot/hardware/UltrasonicSerial.java
+++ src-java/src/team3966/robot/hardware/UltrasonicSerial.java
@@ -4,10 +4,27 @@ import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 
 public class UltrasonicSerial {
-		public String PortReadout () {
-			SerialPort serial = new SerialPort(9600, Port.kOnboard);
-			String readout = serial.readString();
-			return readout;
+	SerialPort serial = null;
+
+	public String PortReadout() {
+		try {
+			if (serial == null) {
+				serial = new SerialPort(9600, Port.kOnboard);
+				serial.setTimeout(0.020);
+			}
+			// byte [] buf = serial.read(8);
+			// return new String(buf);
+
+			if (serial.getBytesReceived() > 0) {
+
+				String readout = serial.readString();
+				return readout;
+			} else {
+				return "No data";
+			}
+		} catch (Exception e) {
+			return "Got an error";
 		}
-		
+	}
+
 }
