894222dba3fbe3cc435580b2e84e6573958e4e12
Tyler-Duckworth <tyler.duck02@gmail.com>
2017-02-27 20:29:31 -0500

    Altered the left autonomous class

--- project/src/team3966/robot/commands/AutonomousLeft.java
+++ project/src/team3966/robot/commands/AutonomousLeft.java
@@ -13,34 +13,34 @@ package team3966.robot.commands;
 
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
-
-
+import edu.wpi.first.wpilibj.command.WaitCommand;
+import team3966.robot.subsystems.Subsystems;
 /**
  *
  * @author Ethan Duckworth
  */
 public class AutonomousLeft extends CommandGroup {
+    public AutonomousLeft() {
+        Subsystems subsystems = new Subsystems();
+        WaitCommand wait = new WaitCommand(10);
 
-    public AutonomousLeft() {/*
-        addSequential(new TankDriveDistance(2.928366, 2.928366));
-        addSequential(new TankDriveAngle(60));*/
-        //addSequential(new AlignToGearPeg());
+        addSequential(new TankDriveDistance(2.30516, 2.30516));
+        addSequential(new TankDriveAngle(60));
+        addSequential(new AlignToGearPeg());
         addSequential(new MoveToGearPeg());
-        //addSequential(new TankDriveDistance(1.935734, 1.935734));
-        // Thread.sleep(2000);
-        // for deliver
-        /*
+        addSequential(new TankDriveDistance(1.357884, 1.357884));
+        subsystems.drive.gate.enable();
         addSequential(new DoNothing(2));
-        addSequential(new TankDriveDistance(-1.464183, -1.464183));
+        addSequential(new TankDriveDistance(-0.88633, -0.886333));
+        subsystems.drive.gate.disable();
         addSequential(new TankDriveAngle(30));
-        addSequential(new TankDriveDistance(4.4704, 4.4704));
+        addSequential(new TankDriveDistance(3.89255, 3.89255));
         addSequential(new TankDriveAngle(45));
         //addSequential(align to boiler tape);
-        //addSequential(shooter control);
+        //addSequential(subsystems.drive.shooter.set(1));
         addSequential(new TankDriveAngle(-135));
-        addSequential(new TankDriveDistance(1.583817, 1.583817));
+        addSequential(new TankDriveDistance(1.005967, 1.005967));
         addSequential(new TankDriveAngle(90));
-        addSequential(new TankDriveDistance(1.27, 1.27));
-*/
+        addSequential(new TankDriveDistance(0.69215, 0.69215));
     }
 }
